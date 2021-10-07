d0e78022e2d3ab422c41e0deefe744eb340ece40
Ismail Aidar <ismailaidar@gmail.com>
2021-10-07 08:17:38 -0400

    protect some pages, only for auth or admin

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,7 @@
 class PostsController < ApplicationController
   before_action :set_post, only: %i[ show edit update destroy ]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :is_admin!, except: [:show, :index]
 
   # GET /posts or /posts.json
   def index
